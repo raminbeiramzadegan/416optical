adb3683bb0fbe05c5e5de8cce4b295bfe4f1aac8
ramin beiramzadegan <raminbeiramzadegan@ramins-MacBook-Pro.local>
2025-05-16 16:01:33 -0400

    commit 7

--- optical/settings.py
+++ optical/settings.py
@@ -42,10 +42,15 @@ INSTALLED_APPS = [
     
 ]
 
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
+
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
-    # "whitenoise.middleware.WhiteNoiseMiddleware",  
+    "whitenoise.middleware.WhiteNoiseMiddleware",  
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
