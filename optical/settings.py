649605e0771431057fc727dd4efda52b97a2b88b
ramin beiramzadegan <raminbeiramzadegan@ramins-MacBook-Pro.local>
2025-05-17 14:50:10 -0400

    Initial commit 10

--- optical/settings.py
+++ optical/settings.py
@@ -82,10 +82,10 @@ WSGI_APPLICATION = "optical.wsgi.application"
 
 # Database
 DATABASES = {
-    'default': dj_database_url.config(
-        default='sqlite:///' + str(BASE_DIR / 'db.sqlite3'),
-        conn_max_age=600
-    )
+   "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
 }
 
 # Password validation
